19736c6d8c5488e535ef1b352edc1c2bb5a38227
Ulrich Schmidt <admin@deruli.de>
2017-09-07 18:19:29 +0200

    fehlender punkt hinzugefügt

--- src/content/modules/charmap/lang/de.php
+++ src/content/modules/charmap/lang/de.php
@@ -1,3 +1,3 @@
 <?php
 define("TRANSLATION_CHARMAP", "Zeichentabelle");
-define("TRANSLATION_CHARMAP_HELP", "Halten Sie auf ein Zeichen getippt, um ein Zeichen in die Zwischenablage kopieren zu können");
+define("TRANSLATION_CHARMAP_HELP", "Halten Sie auf ein Zeichen getippt, um ein Zeichen in die Zwischenablage kopieren zu können.");
